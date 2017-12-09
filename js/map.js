893197ee12892d653069f87f1758f0af890caec1
Alex Resiga <resiga_alex@yahoo.com>
2017-12-09 17:50:21 +0200

    add modal window over map

--- js/map.js
+++ js/map.js
@@ -2,9 +2,9 @@ var map;
 var locationMarker;
 
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 46.774241, lng: 23.594072};
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
+        zoom: 15,
         center: uluru
     });
 
