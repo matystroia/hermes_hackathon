c00b40b9350955487a07b1080b14a66a890ddca7
matt <stroiamatei@gmail.com>
2017-12-10 06:32:59 +0200

    leaderboard and other

--- js/user.js
+++ js/user.js
@@ -15,24 +15,16 @@ function addExp(amount) {
     $('#lvl').text('Level ' + user.level);
     $('#pbar').css('width' , (user.experience / user.levelExp * 100) + '%');
 
-    firebase.auth().onAuthStateChanged(function(fuser) {
-        if (fuser) {
-            var uid = fuser.uid;
-            fuser.getIdToken().then(function(accessToken) {
-                document.getElementById('user-photo').src = fuser.photoURL;
-                //document.getElementById('user-photo').style.display = 'block';
-                document.getElementById('user-name').textContent = displayName;
-                //document.getElementById('user-email').textContent = email;
-                firebase.database().ref('users/' + uid).set({s:'test'});
-            });
-        } else {
-            document.getElementById('user-photo').style.display = 'none';
-            document.getElementById('user-name').textContent = 'No user signed in.';
-            //document.getElementById('user-email').textContent = 'no email provided';
-        }
-    }, function(error) {
-        console.log(error);
-    });
+
+
+    firebase.database().ref('users/' + user.uid).set(
+        {
+            level: user.level,
+            experience: user.experience,
+            levelExp: user.levelExp,
+            name: user.name
+        });
+
 }
 
 function unlockBadge(badgeIndex) {
